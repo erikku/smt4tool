c374779f2a81136e1024d02cb4136518ba50be1e
John Eric Martin <john.eric.martin@gmail.com>
2013-06-20 21:44:57 -0600

    Added the rest of the Wild Bird tribe.

---
+++ database/demons/wild_bird/gu_huo_niao.js
@@ -0,0 +1,32 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "妖鳥",
+	"nameJP": "コカクチョウ",
+	"nameEN": "Gu Huo Niao",
+	"level": 16,
+	"stats": {
+		"hp": 122,
+		"mp": 117,
+		"strength": 20,
+		"skill": 22,
+		"magic": 27,
+		"speed": 19,
+		"luck": 25
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "reflect",
+		"fire": "-",
+		"ice": "weak",
+		"thunder": "-",
+		"shock": "resist",
+		"banish": "-",
+		"curse": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"アギラオ": 0,
+		"パトラ": 0,
+		"子守唄": 18
+	}
+});
---
+++ database/demons/wild_bird/tuofei.js
@@ -0,0 +1,31 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "妖鳥",
+	"nameJP": "タクヒ",
+	"nameEN": "Tuofei",
+	"level": 29,
+	"stats": {
+		"hp": 239,
+		"mp": 121,
+		"strength": 32,
+		"skill": 29,
+		"magic": 29,
+		"speed": 39,
+		"luck": 32
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "weak",
+		"fire": "-",
+		"ice": "-",
+		"thunder": "reflect",
+		"shock": "-",
+		"banish": "-",
+		"curse": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"アイスブレス": 0,
+		"メディア": 30
+	}
+});
