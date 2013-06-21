d5b1daa596324d453b382f75e539cd016c929fdd
John Eric Martin <john.eric.martin@gmail.com>
2013-06-21 00:29:50 -0600

    Added the Heavenly Woman tribe.

---
+++ database/demons/heavenly_woman/sarasvati.js
@@ -0,0 +1,32 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "天女",
+	"nameJP": "サラスヴァティ",
+	"nameEN": "Sarasvati",
+	"level": 48,
+	"stats": {
+		"hp": 273,
+		"mp": 264,
+		"strength": 47,
+		"skill": 52,
+		"magic": 67,
+		"speed": 44,
+		"luck": 62
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "-",
+		"fire": "weak",
+		"ice": "reflect",
+		"thunder": "-",
+		"shock": "-",
+		"banish": "null",
+		"curse": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"ブフダイン": 0,
+		"ラクンダ": 0,
+		"マハブフダイン": 50
+	}
+});
