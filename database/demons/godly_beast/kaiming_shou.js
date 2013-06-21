d712a6c820f5c0c21e2ce5cdb2a7c550ec21a2ac
John Eric Martin <john.eric.martin@gmail.com>
2013-06-21 13:26:43 -0600

    Added the Godly Beast tribe.

---
+++ database/demons/godly_beast/kaiming_shou.js
@@ -0,0 +1,32 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "神獣",
+	"nameJP": "カイメイジュウ",
+	"nameEN": "Kaiming Shou",
+	"level": 41,
+	"stats": {
+		"hp": 482,
+		"mp": 95,
+		"strength": 58,
+		"skill": 52,
+		"magic": 41,
+		"speed": 47,
+		"luck": 39
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "-",
+		"fire": "-",
+		"ice": "-",
+		"thunder": "-",
+		"shock": "-",
+		"banish": "null",
+		"curse": "weak"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"デカジャ": 0,
+		"モータルジハード": 0,
+		"物理無効": 45
+	}
+});
---
+++ database/demons/godly_beast/ukano_mitama.js
@@ -0,0 +1,32 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "神獣",
+	"nameJP": "ウカノミタマ",
+	"nameEN": "Ukano Mitama",
+	"level": 53,
+	"stats": {
+		"hp": 441,
+		"mp": 198,
+		"strength": 59,
+		"skill": 54,
+		"magic": 54,
+		"speed": 71,
+		"luck": 59
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "-",
+		"fire": "-",
+		"ice": "weak",
+		"thunder": "-",
+		"shock": "-",
+		"banish": "-",
+		"curse": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"メディラマ": 0,
+		"道具の知恵・癒": 0,
+		"回復ギガプロレマ": 54
+	}
+});
