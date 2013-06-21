565c07015ad86c73daa912564e0f63f7cdc1d782
John Eric Martin <john.eric.martin@gmail.com>
2013-06-21 14:08:48 -0600

    Added the Holy Beast tribe.

---
+++ database/demons/holy_beast/byakko.js
@@ -0,0 +1,33 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "聖獣",
+	"nameJP": "ビャッコ",
+	"nameEN": "Byakko",
+	"level": 52,
+	"stats": {
+		"hp": 586,
+		"mp": 117,
+		"strength": 72,
+		"skill": 64,
+		"magic": 50,
+		"speed": 58,
+		"luck": 47
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "-",
+		"fire": "-",
+		"ice": "-",
+		"thunder": "absorb",
+		"shock": "weak",
+		"banish": "null",
+		"curse": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"ジオダイン": 0,
+		"電撃ギガプレロマ": 0,
+		"雄叫び": 53,
+		"狂乱の剛爪": 54
+	}
+});
---
+++ database/demons/holy_beast/qing_niuguai.js
@@ -0,0 +1,32 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "聖獣",
+	"nameJP": "セイギュウカイ",
+	"nameEN": "Qing Niuguai",
+	"level": 33,
+	"stats": {
+		"hp": 407,
+		"mp": 81,
+		"strength": 48,
+		"skill": 43,
+		"magic": 34,
+		"speed": 39,
+		"luck": 32
+	},
+	"affinity": {
+		"physical": "resist",
+		"gun": "-",
+		"fire": "-",
+		"ice": "-",
+		"thunder": "-",
+		"shock": "-",
+		"banish": "-",
+		"curse": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"ラクカジャ": 0,
+		"メガトンプレス": 0,
+		"静寂の祈り": 34
+	}
+});
