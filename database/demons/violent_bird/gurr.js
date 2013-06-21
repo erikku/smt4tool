9552c0b6d366b6651ceb2b819972fb08a1ca5dd5
John Eric Martin <john.eric.martin@gmail.com>
2013-06-21 10:52:43 -0600

    Added the Violent Bird tribe.

---
+++ database/demons/violent_bird/gurr.js
@@ -0,0 +1,32 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "凶鳥",
+	"nameJP": "グルル",
+	"nameEN": "Gurr",
+	"level": 43,
+	"stats": {
+		"hp": 371,
+		"mp": 166,
+		"strength": 49,
+		"skill": 45,
+		"magic": 45,
+		"speed": 59,
+		"luck": 49
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "-",
+		"fire": "-",
+		"ice": "-",
+		"thunder": "-",
+		"shock": "reflect",
+		"banish": "weak",
+		"curse": "null"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"マハザンマ": 0,
+		"アクセルクロー": 0,
+		"虚空爪激": 45
+	}
+});
