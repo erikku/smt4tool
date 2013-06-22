19f4625d5cb3a7f0a04581f5821cf6ed17c7c16b
John Eric Martin <john.eric.martin@gmail.com>
2013-06-21 19:36:42 -0600

    Added the Earth Spirit tribe.

---
+++ database/demons/earth_spirit/tsuchigumo.js
@@ -0,0 +1,32 @@
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "地霊",
+	"nameJP": "ツチグモ",
+	"nameEN": "Tsuchigumo",
+	"level": 30,
+	"stats": {
+		"hp": 280,
+		"mp": 126,
+		"strength": 36,
+		"skill": 36,
+		"magic": 36,
+		"speed": 36,
+		"luck": 36
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "-",
+		"fire": "-",
+		"ice": "weak",
+		"thunder": "reflect",
+		"shock": "-",
+		"banish": "-",
+		"curse": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"ポイズンクロー": 0,
+		"ジオンガ": 0,
+		"メパトラ": 31
+	}
+});
