61040ff02ff2fe93f9d46684039722ac4ce75775
John Eric Martin <john.eric.martin@gmail.com>
2013-06-24 16:24:53 -0600

    Combined the demons into a single file for faster loading.

---
+++ database/demons/combine.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+out = open("demons.js", "wb")
+
+for root, dirs, files in os.walk("."):
+	for f in files:
+		if f.endswith(".js") and f != "demons.js":
+			out.write(open(os.path.join(root, f), "rb").read())
