bdb56d624700b20b45cb145eb7552ccdde6eeaa1
John Eric Martin <john.eric.martin@gmail.com>
2013-07-20 17:32:43 -0600

    Combined the changes to the demons.

--- database/demons/demons.js
+++ database/demons/demons.js
@@ -2002,6 +2002,44 @@ registerDemon({
 registerDemon({
 	// This is a mapping for fusion, must stay in Japanese.
 	"tribe": "魔王",
+	"nameJP": "ミトラス",
+	"nameEN": "Mithras",
+	"level": 21,
+	"stats": {
+		"hp": 293,
+		"mp": 58,
+		"strength": 33,
+		"skill": 30,
+		"magic": 24,
+		"speed": 27,
+		"luck": 23
+	},
+	"affinity": {
+		"physical": "-",
+		"gun": "-",
+		"fire": "protect",
+		"ice": "weak",
+		"thunder": "-",
+		"shock": "-",
+		"banish": "null",
+		"curse": "-",
+		"bind": "-",
+		"sleep": "-",
+		"cold": "-",
+		"confusion": "-",
+		"poison": "-"
+	},
+	"skills": {
+		// This is a mapping for skill information, must stay in Japanese.
+		"マハンマ": 0,
+		"ヒートウェイブ": 0,
+		"ラクンダ": 22,
+		"絶命剣": 23
+	}
+});
+registerDemon({
+	// This is a mapping for fusion, must stay in Japanese.
+	"tribe": "魔王",
 	"nameJP": "ツィツィミトル",
 	"nameEN": "Tzitzimitl",
 	"level": 75,
@@ -2034,44 +2072,6 @@ registerDemon({
 		"メギドラ": 0,
 		"エナジードレイン": 0,
 		"電撃反射": 78
-	}
-});
-registerDemon({
-	// This is a mapping for fusion, must stay in Japanese.
-	"tribe": "魔王",
-	"nameJP": "ミトラス",
-	"nameEN": "Mithra",
-	"level": 21,
-	"stats": {
-		"hp": 293,
-		"mp": 58,
-		"strength": 33,
-		"skill": 30,
-		"magic": 24,
-		"speed": 27,
-		"luck": 23
-	},
-	"affinity": {
-		"physical": "-",
-		"gun": "-",
-		"fire": "protect",
-		"ice": "weak",
-		"thunder": "-",
-		"shock": "-",
-		"banish": "null",
-		"curse": "-",
-		"bind": "-",
-		"sleep": "-",
-		"cold": "-",
-		"confusion": "-",
-		"poison": "-"
-	},
-	"skills": {
-		// This is a mapping for skill information, must stay in Japanese.
-		"マハンマ": 0,
-		"ヒートウェイブ": 0,
-		"ラクンダ": 22,
-		"絶命剣": 23
 	}
 });
 registerDemon({
