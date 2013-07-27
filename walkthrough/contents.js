3fe5d00758bc44f0cf32a6e6616fd32470ed8b30
John Eric Martin <john.eric.martin@gmail.com>
2013-07-26 18:10:39 -0600

    Needed this too.

--- walkthrough/contents.js
+++ walkthrough/contents.js
@@ -1,5 +1,7 @@
-$('#walkthrough').html("<h1>Main Story</h1><ol>" +
-	"<li><a onClick=\"walkthroughPage('intro');\">Introduction</a></li>" +
-	"<li><a onClick=\"walkthroughPage('training');\">Training Exercise</a></li>" +
-	"<li><a onClick=\"walkthroughPage('cameron_building');\">Quest: Help Me Get Some Servers</a></li>" +
-"</ol>");
+$("#walkthrough").empty().append($.create("h1").text("Main Story")).append(
+$.create("ol"
+	).contents("Introduction", "intro"
+	).contents("Training Exercise", "training"
+	/*).contents("Toyosu", "toyosu"*/
+	).contents("Quest: Help Me Get Some Servers", "cameron_building"
+));
